15085c492cad297505e69193da113d54945bd2d3
SkazuHD <voda0110@gmail.com>
2023-05-16 14:35:44 +0200

    Base Chart

--- js/main.js
+++ js/main.js
@@ -3,22 +3,68 @@ const ctx = document.getElementById('myChart');
 
 const data = {
   datasets: [{
-    label: 'First Dataset',
+    label: 'CO2',
     data: [{
-      x: 20,
-      y: 30,
-      r: 15
-    }, {
-      x: 40,
-      y: 10,
-      r: 10
+      x: 50,
+      y: 50,
+      r: 150
     }],
-    backgroundColor: 'rgb(255, 99, 132)'
-  }]
+    backgroundColor: 'rgba(105,105,105,0.3)'
+  }, {
+    label: 'Water',
+    data: [{
+      x: 50,
+      y: 50,
+      r: 100
+    }],
+    backgroundColor: 'rgb(54, 162, 235)'
+    },
+    {
+      label: 'Ice',
+      data: [{
+        x: 0,
+        y: 0,
+        r: 75
+      }, {
+        x: 100,
+        y: 100,
+        r: 75
+      }],
+      backgroundColor: 'rgb(159,187,208)'
+    }]
 };
 const config = {
   type: 'bubble',
   data: data,
-  options: {}
+  options: {
+    plugins: {
+      title:
+      {
+        display: true,
+        text: 'THE MELTING PROBLEM',
+        font: {
+          size: 64,
+        }
+
+      },
+      legend: {
+        display: false,
+      }
+    },
+    scales: {
+      y: {
+        display: false,
+        grid: {
+          display: false,
+        },
+      },
+      x: {
+        display: false,
+        grid: {
+          display: false,
+        },
+      },
+    },
+  }
 };
 new Chart(ctx, config);
